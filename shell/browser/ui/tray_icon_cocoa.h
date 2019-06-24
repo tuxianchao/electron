c1658fc7be42262e94b07a41851cdf8d63f44c57
Micha Hanselmann <deermichel@github.com>
2019-06-24 16:55:39 -0700

    some things work others dont

--- shell/browser/ui/tray_icon_cocoa.h
+++ shell/browser/ui/tray_icon_cocoa.h
@@ -12,7 +12,7 @@
 #include "base/mac/scoped_nsobject.h"
 #include "shell/browser/ui/tray_icon.h"
 
-// @class AtomMenuController;
+@class AtomMenuController;
 @class StatusItemView;
 
 namespace electron {
@@ -45,10 +45,10 @@ class TrayIconCocoa : public TrayIcon, public AtomMenuModel::Observer {
   base::scoped_nsobject<StatusItemView> status_item_view_;
 
   // Status menu shown when right-clicking the system icon.
-  // base::scoped_nsobject<AtomMenuController> menu_;
+  base::scoped_nsobject<AtomMenuController> menu_;
 
   // Used for unregistering observer.
-  // AtomMenuModel* menu_model_ = nullptr;  // weak ref.
+  AtomMenuModel* menu_model_ = nullptr;  // weak ref.
 
   base::WeakPtrFactory<TrayIconCocoa> weak_factory_;
 
