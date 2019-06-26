c8aa64ec160c73731b4115c0b217aee890a7dd85
Micha Hanselmann <deermichel@github.com>
2019-06-26 11:35:08 -0700

    add imports

--- shell/browser/ui/tray_icon_cocoa.mm
+++ shell/browser/ui/tray_icon_cocoa.mm
@@ -7,7 +7,11 @@
 #include <string>
 #include <vector>
 
+#include "base/message_loop/message_loop.h"
 #include "base/strings/sys_string_conversions.h"
+#include "base/task/post_task.h"
+#include "content/public/browser/browser_task_traits.h"
+#include "content/public/browser/browser_thread.h"
 #include "shell/browser/ui/cocoa/NSString+ANSI.h"
 #include "shell/browser/ui/cocoa/atom_menu_controller.h"
 #include "ui/events/cocoa/cocoa_event_utils.h"
